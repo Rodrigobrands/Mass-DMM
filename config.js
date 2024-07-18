ff5e8bd83beca428d2133f5c3a8a5c8aa82435d2
Rodrigobrands <155950369+Rodrigobrands@users.noreply.github.com>
2024-07-18 06:05:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    token: "MTI0ODM5NjE4Nzg1MDExNzIwMw"  // place your token here
+    token: "MTI0ODM5NjE4Nzg1MDExNzIwMw.GE2lYx.NlpzYI9w7FGP1_7dhSMCIjXvFCe_90tfBxug6w"  // place your token here
 }
